3fa5f26494994b6d224c165c45818073ac186c93
Katri Pokats <katri.pokats@gmail.com>
2023-04-24 15:39:36 +0300

    Add data class Person

---
+++ src/main/java/com/project/personDemo/Person.java
@@ -0,0 +1,31 @@
+package com.project.personDemo;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "first_name", nullable = false, length = Integer.MAX_VALUE)
+    private String firstName;
+
+    @NotNull
+    @Column(name = "last_name", nullable = false, length = Integer.MAX_VALUE)
+    private String lastName;
+
+    @NotNull
+    @Column(name = "birth_date", nullable = false)
+    private LocalDate birthDate;
+}
