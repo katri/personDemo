ecbc31d088d2736ec2d8ad9cfb08cbb191c64f11
Katri Pokats <katri.pokats@gmail.com>
2023-04-25 15:41:04 +0300

    Update path

--- src/main/java/com/project/personDemo/PersonController.java
+++ src/main/java/com/project/personDemo/PersonController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("persons/")
+@RequestMapping("persons")
 public class PersonController {
     @Resource
     private PersonService personService;
