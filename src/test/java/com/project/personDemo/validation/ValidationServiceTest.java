35a2e29edc5bbf90716d6c0455a9aab767fc2d7f
Katri Pokats <katri.pokats@gmail.com>
2023-04-26 11:45:07 +0300

    Add tests for id Validation

--- src/test/java/com/project/personDemo/validation/ValidationServiceTest.java
+++ src/test/java/com/project/personDemo/validation/ValidationServiceTest.java
@@ -1,6 +1,7 @@
 package com.project.personDemo.validation;
 
 import com.project.personDemo.infrastructure.exception.DateException;
+import com.project.personDemo.infrastructure.exception.IdWrongFormatException;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -24,5 +25,43 @@ class ValidationServiceTest {
                 -> ValidationService.validateBirthDate(birthdate));
         assertTrue(dateException.getMessage()
                 .contentEquals("Sünnikuupäev on liiga kaugel. Palun sisesta sünnikuupäev alates 01.01.2000"));
+    }
+
+    @Test
+    void validateIdWithCorrectData() {
+        String idAsString = "2";
+        Integer idAsInteger = ValidationService.validateId(idAsString);
+
+        assertEquals(idAsInteger, 2);
+    }
+
+    @Test
+    void validateIdWithStringAsData() {
+        String idAsString = "foo";
+
+        IdWrongFormatException formatException = assertThrows(IdWrongFormatException.class, ()
+                -> ValidationService.validateId(idAsString));
+        assertTrue(formatException.getMessage().contentEquals("ID on vales formaadis"));
+        assertTrue(formatException.getDetail().contentEquals("foo"));
+    }
+
+    @Test
+    void validateNullId() {
+        String idAsString = null;
+
+        IdWrongFormatException formatException = assertThrows(IdWrongFormatException.class, ()
+                -> ValidationService.validateId(idAsString));
+        assertTrue(formatException.getMessage().contentEquals("ID on vales formaadis"));
+        assertTrue(formatException.getDetail().contentEquals("id on puudu"));
+    }
+
+    @Test
+    void validateEmptyId() {
+        String idAsString = "";
+
+        IdWrongFormatException formatException = assertThrows(IdWrongFormatException.class, ()
+                -> ValidationService.validateId(idAsString));
+        assertTrue(formatException.getMessage().contentEquals("ID on vales formaadis"));
+        assertTrue(formatException.getDetail().contentEquals(""));
     }
 }
